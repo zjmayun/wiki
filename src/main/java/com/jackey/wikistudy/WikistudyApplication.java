18b311b73c76453b630a3249a830bbb92530c779
2268232834@qq.com <Zj18390096456->
2021-11-19 16:46:13 +0800

    第一次提交代码

---
+++ src/main/java/com/jackey/wikistudy/WikistudyApplication.java
@@ -0,0 +1,13 @@
+package com.jackey.wikistudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikistudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikistudyApplication.class, args);
+    }
+
+}
