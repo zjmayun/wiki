a81ec782afb5a80ebbbd8c788e2eaee4fcb41721
2268232834@qq.com <Zj18390096456->
2021-11-21 14:57:07 +0800

    首页页面的书写

--- wikivue/src/App.vue
+++ wikivue/src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <a-layout>
+      <a-layout-header class="header">
+        <div class="logo" />
+        <a-menu
+            v-model:selectedKeys="selectedKeys1"
+            theme="dark"
+            mode="horizontal"
+            :style="{ lineHeight: '64px' }"
+        >
+          <a-menu-item key="1">nav 1</a-menu-item>
+          <a-menu-item key="2">nav 2</a-menu-item>
+          <a-menu-item key="3">nav 3</a-menu-item>
+        </a-menu>
+      </a-layout-header>
+      <router-view/>
+      <a-layout-footer style="text-align: center">
+         wiki ©2021 Created by Jackey
+      </a-layout-footer>
+    </a-layout>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
