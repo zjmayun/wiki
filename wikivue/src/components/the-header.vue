068abfbd39f67392b049038611defb99a51c2e3c
uzi <2268232834@qq.com>
2021-11-24 10:37:25 +0800

    增加首页导航 router-link的使用

--- wikivue/src/components/the-header.vue
+++ wikivue/src/components/the-header.vue
@@ -7,9 +7,15 @@
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="12">nav 2</a-menu-item>
-      <a-menu-item key="22">nav 3</a-menu-item>
-      <a-menu-item key="32">nav 4</a-menu-item>
+      <a-menu-item key="/">
+        <router-link to="/">首页</router-link> |
+      </a-menu-item>
+      <a-menu-item key="/admin/ebook">
+        <router-link to="/admin/ebook">电子书管理</router-link> |
+      </a-menu-item>
+      <a-menu-item key="/about">
+        <router-link to="/about">关于我们</router-link> |
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
